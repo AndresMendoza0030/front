6f7049b7ae1207d2e1160ecd22dcda1a4bf53db2
Andres <00305422@uca.edu.sv>
2024-10-05 05:34:49 -0600

    'a'

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { GoogleOAuthProvider, googleLogout, useGoogleLogin } from '@react-oauth/google'; // Importamos GoogleOAuthProvider y useGoogleLogin
 import { useAuth } from '../context/AuthContext';
 import { usePermissions } from '../context/PermissionsContext';
 import { useNavigate } from 'react-router-dom';
@@ -44,8 +45,39 @@ const Login = () => {
         }
     };
 
-    const handleGoogleLogin = () => {
-        window.location.href = 'https://backend-production-5e0d.up.railway.app/auth/google/';
+    const handleGoogleSuccess = async (tokenResponse) => {
+        try {
+            const idToken = tokenResponse.credential;
+
+            const result = await fetch('https://backend-production-5e0d.up.railway.app/auth/google/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ idToken }),
+            });
+
+            const data = await result.json();
+
+            console.log('Datos recibidos del servidor (Google):', data);
+
+            if (result.ok && data.data.token) {
+                const { token, user, permissions, roles } = data.data;
+                setPermissions(permissions);
+                login(roles, token, user.name);
+                navigate('/dashboard');
+            } else {
+                throw new Error('Error al autenticar con la API');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Error al iniciar sesión con Google');
+        }
+    };
+
+    const handleGoogleFailure = (error) => {
+        console.error('Error al iniciar sesión con Google:', error);
+        alert('Error al iniciar sesión con Google');
     };
 
     const handlePasswordRecovery = () => {
@@ -53,41 +85,44 @@ const Login = () => {
     };
 
     return (
-        <div className="login-container">
-            <img src="images/logo.webp" alt="Logo de la Organización" />
-            <h1>Bienvenid@ al Portal</h1>
-            <p>Inicio de Sesión</p>
-            <form onSubmit={handleLogin}>
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Correo electrónico"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Contraseña"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button type="submit">Iniciar Sesión</button>
-            </form>
-            <button onClick={handleGoogleLogin} className="google-login-button">
-                <div className="google-icon-wrapper">
-                    <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/4/4a/Logo_2013_Google.png" alt="Google Icon" />
+        <GoogleOAuthProvider clientId="3191715525-54lsdrhbk22k0dk2e6cdrlqk2derqcbj.apps.googleusercontent.com">
+            <div className="login-container">
+                <img src="images/logo.webp" alt="Logo de la Organización" />
+                <h1>Bienvenid@ al Portal</h1>
+                <p>Inicio de Sesión</p>
+                <form onSubmit={handleLogin}>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Correo electrónico"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Contraseña"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <button type="submit">Iniciar Sesión</button>
+                </form>
+                <div className="google-login-button">
+                    <useGoogleLogin
+                        onSuccess={handleGoogleSuccess}
+                        onFailure={handleGoogleFailure}
+                        useOneTap
+                    />
                 </div>
-                <span className="button-text">Iniciar sesión con Google</span>
-            </button>
-            <p>
-                <button onClick={handlePasswordRecovery} className="link-button">
-                    ¿Olvidó su contraseña?
-                </button>
-            </p>
-        </div>
+                <p>
+                    <button onClick={handlePasswordRecovery} className="link-button">
+                        ¿Olvidó su contraseña?
+                    </button>
+                </p>
+            </div>
+        </GoogleOAuthProvider>
     );
 };
 
